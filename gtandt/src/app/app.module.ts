17b38d8d7037b8488351026296a57ee03ba53496
joelmatt <joel99cherian@gmail.com>
2020-10-25 01:30:35 +0530

    Added Vertical SideNav Bar (Responsive) :iphone:

--- gtandt/src/app/app.module.ts
+++ gtandt/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatIconModule } from "@angular/material/icon";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { RecruitmentsComponent } from './recruitments/recruitments.component';
+import { RequirementsComponent } from './requirements/requirements.component';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    CandidatesComponent,
+    SponsorsComponent,
+    RecruitmentsComponent,
+    RequirementsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatDividerModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
