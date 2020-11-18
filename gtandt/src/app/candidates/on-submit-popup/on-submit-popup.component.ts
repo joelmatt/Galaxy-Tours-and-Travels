743edc3b9dbcc65491fb17df397969e4940e9c96
joelmatt <joel99cherian@gmail.com>
2020-11-19 00:16:32 +0530

    :ocean: :ocean: added another class for delete popup :ocean: :ocean:

--- gtandt/src/app/candidates/on-submit-popup/on-submit-popup.component.ts
+++ gtandt/src/app/candidates/on-submit-popup/on-submit-popup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef  } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-on-submit-popup',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnSubmitPopupComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data, public dialogRef: MatDialogRef<OnSubmitPopupComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  closeDialog() {
+    this.dialogRef.close(false);
   }
 
 }
