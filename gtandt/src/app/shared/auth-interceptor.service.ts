fd07ac8d787365d3c416ea24fb203480595b182f
joelmatt <joel99cherian@gmail.com>
2020-12-14 13:44:39 +0530

    :last_quarter_moon: added if clauses to the interceptor 	:last_quarter_moon:

--- gtandt/src/app/shared/auth-interceptor.service.ts
+++ gtandt/src/app/shared/auth-interceptor.service.ts
@@ -7,13 +7,17 @@ import { exhaustMap, take } from 'rxjs/operators';
 export class AuthInterceptorService implements HttpInterceptor{
     constructor(private authService: AuthService){}
     intercept(req: HttpRequest<any>, next: HttpHandler){
-        
-        return this.authService.user.pipe(
-            take(1),
-            exhaustMap(user => {
-                const modifiedReq = req.clone({headers: req.headers.append('X-Amz-Security-Token', user.getIdToken)});
-                return next.handle(modifiedReq);
-            })
-        );
+        if(!req.params.get('funcName')){
+            return next.handle(req);
+        }
+        else{
+            return this.authService.user.pipe(
+                take(1),
+                exhaustMap(user => {
+                    const modifiedReq = req.clone({headers: req.headers.append('X-Amz-Security-Token', user.getIdToken)});
+                    return next.handle(modifiedReq);
+                })
+            );
+        }
     }
 }
