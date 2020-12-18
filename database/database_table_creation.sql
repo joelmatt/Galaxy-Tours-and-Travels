c3a055a9f81624f2b280c947f29fbe4134abcadd
joelmatt <joel99cherian@gmail.com>
2020-12-18 20:02:50 +0530

    :bullettrain_front: :bullettrain_front: Minor Database Update :bullettrain_front: :bullettrain_front:

--- database/database_table_creation.sql
+++ database/database_table_creation.sql
@@ -1,7 +1,7 @@
-# database_name: galaxytnt
+-- database_name: galaxytnt
 
 use galaxytnt;
-# candidate information: This table consists of all the primary information regarding the candidates
+-- candidate information: This table consists of all the primary information regarding the candidates
 CREATE TABLE IF NOT EXISTS candidate_info(
 	id INT AUTO_INCREMENT NOT NULL,
 	candidate_id VARCHAR(36) NOT NULL,
@@ -16,45 +16,45 @@ CREATE TABLE IF NOT EXISTS candidate_info(
 	pincode INT NOT NULL,
 	email VARCHAR(50),
 	origin ENUM('online', 'offline') NOT NULL,
-	status ENUM('A', 'NA') NOT NULL, # status determines whether the person is available or Not Availabe a.k.a.  already hired.
+	status ENUM('A', 'NA') NOT NULL, -- status determines whether the person is available or Not Availabe a.k.a.  already hired.
 	biodata VARCHAR(200), -- updated
-	DOE DATE NOT NULL, # Date when the entry was made (use curdate() function)
+	DOE DATE NOT NULL, -- Date when the entry was made (use curdate() function)
 	PRIMARY KEY(candidate_id),
 	INDEX(id)
 );
 
-# specialization list: This table consists of all the different types of specialization offered by the candidates
+-- specialization list: This table consists of all the different types of specialization offered by the candidates
 CREATE TABLE IF NOT EXISTS spec_list(
 	spec_id VARCHAR(36) NOT NULL,
 	specialization VARCHAR(30) NOT NULL,
 	PRIMARY KEY(spec_id)
 );
 
-# candidate specialization junction: This table acts as a junction between the candidiate_info table and
-# spec_list table. 
+-- candidate specialization junction: This table acts as a junction between the candidiate_info table and
+-- spec_list table. 
 CREATE TABLE IF NOT EXISTS candidate_spec_junc(
 	candidate_id VARCHAR(36) NOT NULL,
 	spec_id VARCHAR(36) NOT NULL,
-	experience DECIMAL(4,2) NOT NULL, # e.g. 3.1, 22.5, 2.52 => years before decimal and months after decimal
+	experience DECIMAL(4,2) NOT NULL, -- e.g. 3.1, 22.5, 2.52 => years before decimal and months after decimal
 	PRIMARY KEY(candidate_id, spec_id),
 	FOREIGN KEY(candidate_id) REFERENCES candidate_info(candidate_id) ON DELETE CASCADE,
 	FOREIGN KEY(spec_id) REFERENCES spec_list(spec_id)
 );
 
 
-# passport information: stores candidates passport information
+-- passport information: stores candidates passport information
 CREATE TABLE IF NOT EXISTS passport_info(
 	candidate_id VARCHAR(36) NOT NULL,
 	passport_no VARCHAR(10) NOT NULL,
-	POI VARCHAR(50) NOT NULL,	# 	place of issue (place where passport was issued)
+	POI VARCHAR(50) NOT NULL,	-- 	place of issue (place where passport was issued)
 	DOI DATE NOT NULL,
 	DOE DATE NOT NULL,
-	passport_copy VARCHAR(20),
+	passport_copy VARCHAR(200),
 	PRIMARY KEY(candidate_id), 
 	FOREIGN KEY(candidate_id) REFERENCES candidate_info(candidate_id) ON DELETE CASCADE
 );
 
-# candidate proof: files locations of the acknowledgement letter and video proofs
+-- candidate proof: files locations of the acknowledgement letter and video proofs
 CREATE TABLE IF NOT EXISTS candidate_proof(
 	candidate_id VARCHAR(36) NOT NULL,
 	acc_letter VARCHAR(20),
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS candidate_proof(
 	FOREIGN KEY(candidate_id) REFERENCES candidate_info(candidate_id) ON DELETE CASCADE
 );
 
-# sponsor information: this table consists of details regarding all the sponsors
+-- sponsor information: this table consists of details regarding all the sponsors
 CREATE TABLE IF NOT EXISTS sponsor_info(
 	sponsor_id VARCHAR(36) NOT NULL,
 	name VARCHAR(50) NOT NULL,
@@ -75,15 +75,15 @@ CREATE TABLE IF NOT EXISTS sponsor_info(
 	PRIMARY KEY(sponsor_id)
 );
 
-# visa information: visa related information for each candidate
+-- visa information: visa related information for each candidate
 CREATE TABLE IF NOT EXISTS visa_info(
 	visa_id VARCHAR(36) NOT NULL,
 	sponsor_id VARCHAR(36) NOT NULL,
 	candidate_id VARCHAR(36) NOT NULL,
-	pass_sub_date DATE, # passport submission date
-	pass_col_date DATE, # passport collection date
-	visa_no varchar(50), # visa number
-	visa_copy varchar(20), # file location for the visa copy
+	pass_sub_date DATE, -- passport submission date
+	pass_col_date DATE, -- passport collection date
+	visa_no varchar(50), --  visa number
+	visa_copy varchar(20), -- file location for the visa copy
 	visa_date DATE, 
 	remark varchar(100),
 	PRIMARY KEY(visa_id),
@@ -91,19 +91,19 @@ CREATE TABLE IF NOT EXISTS visa_info(
 	FOREIGN KEY(candidate_id) REFERENCES candidate_info(candidate_id) ON DELETE CASCADE
 );
 
-# recruitment: this table consists of a list of all the recruitment drives created
+-- recruitment: this table consists of a list of all the recruitment drives created
 CREATE TABLE IF NOT EXISTS recruitment_list(
 	recruitment_id VARCHAR(36) NOT NULL,
 	sponsor_id VARCHAR(36) NOT NULL,
 	total_cat INT NOT NULL,
-	DOC DATE NOT NULL, # Date of recruitment drive creation
+	DOC DATE NOT NULL, -- Date of recruitment drive creation
 	recruitment_name VARCHAR(20) NOT NULL, 
 	status ENUM('Active', 'Inactive') NOT NULL,
 	PRIMARY KEY(recruitment_id),
 	FOREIGN KEY(sponsor_id) REFERENCES sponsor_info(sponsor_id)
 );
 
-# recruitment requirement junction: a junction between the recruitment_list table and recruitment_content table
+-- recruitment requirement junction: a junction between the recruitment_list table and recruitment_content table
 CREATE TABLE IF NOT EXISTS recruit_req_junc(
 	recruitment_id VARCHAR(36) NOT NULL,
 	spec_id VARCHAR(36) NOT NULL,
@@ -117,8 +117,8 @@ CREATE TABLE IF NOT EXISTS recruit_req_junc(
 CREATE TABLE IF NOT EXISTS recruitment_content(
 	recruitment_id VARCHAR(36) NOT NULL,
 	candidate_id VARCHAR(36) NOT NULL,
-	shortlist INT NOT NULL, # 1=> shortlisted candidate, 0=> not shortlisted
-	interview INT NOT NULL, # 2=> interveiw selected, 0=> not selected
+	shortlist INT NOT NULL, -- 1=> shortlisted candidate, 0=> not shortlisted
+	interview INT NOT NULL, -- 2=> interveiw selected, 0=> not selected
 	PRIMARY KEY(recruitment_id, candidate_id),
 	FOREIGN KEY(recruitment_id) REFERENCES recruitment_list(recruitment_id),
 	FOREIGN KEY(candidate_id) REFERENCES candidate_info(candidate_id)
