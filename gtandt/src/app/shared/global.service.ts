cb2637d7c422f526cdcdcbedf9d7987368cad3e9
joelmatt <joel99cherian@gmail.com>
2021-01-06 03:25:59 +0530

    :boom::boom: Made pop-up dialogs global :boom::boom:

--- gtandt/src/app/shared/global.service.ts
+++ gtandt/src/app/shared/global.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { OnSubmitPopupComponent } from '../../on-submit-popup/on-submit-popup.component';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +9,30 @@ import { Injectable } from '@angular/core';
 export class GlobalService {
 
   cardTitle: string = "";
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   updateCardTitle(title: string){
     this.cardTitle = title;
+  }
+
+  openSubmitDialog(msg: string, mode){
+    this.dialog.open(OnSubmitPopupComponent, {
+      disableClose: true,
+      hasBackdrop: true,
+      panelClass: 'confirm-dialog-container',
+      data: {message:  msg, mode: mode}
+    });  
+  }
+
+  openDeleteDialog(msg: string, mode){
+    return this.dialog.open(OnSubmitPopupComponent, {
+      disableClose: true,
+      panelClass: 'confirm-dialog-container',
+      data: {message:  msg, mode: mode}
+    });  
+  }
+
+  closeSubmitDialog(){
+    this.dialog.closeAll();
   }
 }
